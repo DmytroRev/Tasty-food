a97524c68a60dd49e991b912ed4b8430f5edce64
Dmytro Reviakin <thefaust31@gmail.com>
2024-11-19 14:59:37 +0200

    create underMain

---
+++ src/components/UnderMain/UnderMain.jsx
@@ -0,0 +1,26 @@
+import css from "./UnderMain.module.css";
+
+const UnderMain = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.containerCardBrocoli}>
+        <div>
+        <h2 className={css.title}>Get 10% off on Vegetables</h2>
+      <p className={css.info}>Shop our selection of organic fresh vegetables in a discounted price. 10% of on all vegetables</p>
+      <button className={css.btnBrocoli}>shop now</button>
+      </div>
+      <img src="/brocoli.png" alt="brocoli" style={{width: 265}}/>
+      </div>
+      <div  className={css.containerCardVegetables}>
+        <div>
+        <h2 className={css.title}>Get 10% off on Vegetables</h2>
+      <p className={css.info}>Shop our selection of organic fresh vegetables in a discounted price. 10% of on all vegetables</p>
+      <button className={css.btnVgtls}>shop now</button>
+      </div>
+      <img src="/vegetables.png" alt="vegetables" style={{width:282}}/>
+      </div>
+    </div>
+  );
+};
+
+export default UnderMain;
