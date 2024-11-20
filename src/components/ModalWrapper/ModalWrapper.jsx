642d1a8be927755b3c14e896607d86fb79cfcd8b
Dmytro Reviakin <thefaust31@gmail.com>
2024-11-20 22:17:44 +0200

    create product list section and design

--- src/components/ModalWrapper/ModalWrapper.jsx
+++ src/components/ModalWrapper/ModalWrapper.jsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
 import PropTypes from "prop-types";
-
+import css from './ModalWrapper.module.css'
+import { Rating } from "@smastrom/react-rating";
+import { IoCloseSharp } from "react-icons/io5";
 
 const customStyles = {
   content: {
@@ -32,17 +34,33 @@ const ModalWrapper = ({ isOpen, onClose, product }) => {
         shouldCloseOnEsc={true}
       >
         {product && (
-          <div>
+          <div className={css.container}>
             <div>
+            <button onClick={onClose} className={css.btnClose}><IoCloseSharp />
+            </button>
+            </div>
+            <div className={css.img}>
               <img src={product.image} alt={product.name} />
             </div>
-            <div>
-              <h2>{product.name}</h2>
-              <p>{product.category}</p>
-              <p>{product.price}</p>
-              <p>{product.discountPrice}</p>
-              <p>{product.rating}</p>
+            <div className={css.containerWithInfo}>
+              <h2 className={css.title}>{product.name}</h2>
+              <p className={css.category}>{product.category}</p>
+              <p className={css.price}>
+          {product.isDiscounted ? (
+            <>
+              <span className={css.oldPrice}>${product.price.toFixed(2)} <span style={{color: '#76A713', fontSize: 15}}>-</span></span>
+              <span className={css.price}> ${product.discountPrice.toFixed(2)}</span>
+            </>
+          ) : (
+            `$${product.price.toFixed(2)}`
+          )}
+        </p>
               <p>{product.description}</p>
+              <div className={css.rating}>
+        <Rating style={{maxWidth: 100}}
+        value={product.rating}
+        readOnly/>
+        </div>
             </div>
           </div>
         )}
@@ -59,6 +77,7 @@ ModalWrapper.propTypes = {
     name: PropTypes.string,
     category: PropTypes.string,
     price: PropTypes.number,
+    isDiscounted: PropTypes.bool,
     discountPrice: PropTypes.number,
     rating: PropTypes.number,
     description: PropTypes.string,
